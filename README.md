602e96d7e505d059e133bd031fa434336781d175
Pavel Semyonov <pavel.semyonov@jetbrains.com>
2020-06-01 13:09:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a tool for checking the completeness of a WebHelp table of content
+comparing to the list of topics in the same WebHelp project.
+
+To check the TOC completeness, run main with two arguments:
+* path to the WebHelp project
+* name of the TOC XML file
+
+For example:
+../kotlin-reference-help/ kr.tree
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,55 @@
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import org.xml.sax.InputSource
+import java.io.File
+import java.io.StringReader
+import javax.xml.parsers.DocumentBuilderFactory
+
+fun main(args: Array<String>) {
+  println("Getting topic files from topics/")
+  val topics = getTopicList(args[0])
+  println("Done")
+  println("Getting toc from .tree file")
+  val toc = getTocAsList(args[0]+ args[1])
+  println("Done")
+
+  val missedTopics = topics - toc
+  println("...\nThe following ${missedTopics.count()} files from topics/ are not present in TOC:")
+  var i = 1
+  for(topic in missedTopics.sorted()) println("${i++}. $topic")
+}
+
+fun getTopicList(path: String): List<String> {
+  val topics = mutableListOf<String>()
+  File("$path/topics").walk().forEach {
+    topics.add(it.name)
+  }
+  return topics.filter { it.endsWith(".md") }
+}
+
+fun getTocAsList(treePath: String): List<String> {
+  val toc = mutableListOf<String>()
+
+  val xmlToc = readXmlToc(treePath)
+  val tocElements = xmlToc.getElementsByTagName("toc-element")
+  for (i in 0 until tocElements.length) {
+    val item = tocElements.item(i)
+    if (item.getNodeType().equals(Node.ELEMENT_NODE)) {
+      //println(item.attributes.getNamedItem("id")?.nodeValue)
+      val idAttr = item.attributes.getNamedItem("id")
+      idAttr ?.let { toc.add(idAttr.nodeValue)}
+    }
+  }
+  return toc
+}
+
+fun readXmlToc(treePath: String): Document {
+  val xmlFile = File("$treePath")
+
+  val dbFactory = DocumentBuilderFactory.newInstance()
+  val dBuilder = dbFactory.newDocumentBuilder()
+  val xmlInput = InputSource(StringReader(xmlFile.readText()))
+  val doc = dBuilder.parse(xmlInput)
+
+  return doc
+}
